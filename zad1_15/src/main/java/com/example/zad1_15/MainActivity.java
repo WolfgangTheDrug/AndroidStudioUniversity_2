787ea3875cfba69fccf8709bddbaba35d9d0cf1f
Wolfgang <bardak_widz@wp.pl>
2021-04-25 22:40:42 +0200

    works fine and is kinda cute

--- zad1_15/src/main/java/com/example/zad1_15/MainActivity.java
+++ zad1_15/src/main/java/com/example/zad1_15/MainActivity.java
@@ -5,47 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
 import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private static ViewPagerFragment[] tabFragm;
-//    private String polishWords[] = {"Komputer", "Monitor", "Klawiatura", "Mysz"};
-//    private String englishWords[] = {"Computer", "Monitor", "Keyboard", "Mouse"};
+    private Button check;
 
+    public String getRandomColor() {
+        int number = 0;
+        String hexValue = "", color = "#";
 
+        for (int i = 0; i < 3; i++) {
+            number = (int) ( Math.random() * Math.pow(16, 2) );
+            hexValue = Integer.toHexString(number);
+            color += hexValue.length() == 1 ? "0" + hexValue : hexValue;
+        }
+        return color;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TypedArray slowka = getResources().obtainTypedArray(R.array.slowka);
+        check = findViewById(R.id.buttonCheck);
+
+        TypedArray slowka = getResources().obtainTypedArray(R.array.dictionary);
         tabFragm = new ViewPagerFragment[slowka.length()];
         for (int i = 0; i < tabFragm.length; i++){
-            // tu wygenerowanie losowego koloru...
-            String temp = slowka.getString(i);
-            int index = temp.indexOf(":");
 
-            String plWord = temp.substring(0, index);
-            String engWord = temp.substring(index + 1);
+            String[] separated = slowka.getString(i).split(":");
+            
+            String polishWord = separated[0];
+            String englishWord = separated[1];
 
-            tabFragm[i] = new ViewPagerFragment("#12345F", plWord, engWord);
+            tabFragm[i] = new ViewPagerFragment(getRandomColor(), polishWord, englishWord);
         }
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
 
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), String.format("%s na %s", ViewPagerFragment.correctCounter, ViewPagerFragment.howMany), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
     }
 
-    ///////////////////////////////////////////
-
     private class MyPagerAdapter extends FragmentStatePagerAdapter {
-
         public MyPagerAdapter(FragmentManager fm) {
             super(fm, FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         }
--- zad1_15/src/main/java/com/example/zad1_15/ViewPagerFragment.java
+++ zad1_15/src/main/java/com/example/zad1_15/ViewPagerFragment.java
@@ -6,25 +6,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ViewPagerFragment extends Fragment {
 
+    private final String textColor;
     private TextView tvPl;
-    private TextView tvAng;
+    private EditText etAng;
     private Button button;
-    private String color;
+    private String backgroundColor;
     private String textPl;
     private String textAng;
     private boolean kliknieto = false;
+    public static int correctCounter = 0;
+    public static int howMany = 5;
 
-    public ViewPagerFragment(String color, String textPl, String textAng) {
+    private String invertHexColor(String hexColor) {
+        char[] hexChar = hexColor.substring(1).toCharArray(); // array sześciu cyfr heksadecymalnych
+        for (int i = 0; i < 3; i++) {
+            int value = Integer.parseInt(String.valueOf(hexChar[2*i]), 16); // wyciąnij co drugą cyfrę
+            value = 0xF - value; // odwróć tę cyfrę względem bazy?
+            hexChar[2*i] = (Integer.toHexString(value)).charAt(0); // zamień starą cyfrę na nową
+        } // nadal mam sześć cyfr
+        return  "#" + new String(hexChar);
+    }
+
+    public ViewPagerFragment(String backgroundColor, String textPl, String textAng) {
         super();
-        this.color = color;
+        this.backgroundColor = backgroundColor;
+        this.textColor = invertHexColor(backgroundColor);
         this.textPl = textPl;
         this.textAng = textAng;
     }
@@ -34,25 +49,33 @@ public class ViewPagerFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_content, container, false);
         tvPl = view.findViewById(R.id.textPL);
-        tvAng = view.findViewById(R.id.textANG);
+        etAng = view.findViewById(R.id.etANG);
         button = view.findViewById(R.id.buttonODP);
 
-        tvPl.setBackgroundColor(Color.parseColor(color));
-        tvAng.setBackgroundColor(Color.parseColor(color));
+        tvPl.setBackgroundColor(Color.parseColor(backgroundColor));
+        etAng.setBackgroundColor(Color.parseColor(backgroundColor));
+        tvPl.setTextColor(Color.parseColor(textColor));
+        etAng.setTextColor(Color.parseColor(textColor));
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tvAng.setText(textAng);
-                kliknieto = true;
+                if (etAng.getText().toString().equals(textAng)) {
+                    Toast toast = Toast.makeText(getContext(), "Bravo!", Toast.LENGTH_SHORT);
+                    toast.show();
+                    if (kliknieto) {
+                        correctCounter += 0;
+                    } else {
+                        correctCounter += 1;
+                        kliknieto = true;
+                    }
+                } else {
+                    Toast toast = Toast.makeText(getContext(), "Spróbuj jeszcze raz!", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
-
         tvPl.setText(textPl);
-
-        if (kliknieto) {
-            tvAng.setText(textAng);
-        }
 
         return view;
     }
