caa454723b95b2cbc40a7ec8e33bae55e8df0dba
Wolfgang <bardak_widz@wp.pl>
2021-04-27 14:29:23 +0200

    almost everything works in zad1_16

--- zad1_16/src/main/java/com/example/zad1_16/MainActivity.java
+++ zad1_16/src/main/java/com/example/zad1_16/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.zad1_16;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ListView listView;
+    private String[] strsMenu = new String[]{"Oferta", "Koszyk", "Wiadomości"};
+    private MyFragment[] tabFr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        listView = findViewById(R.id.left_drawer);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.simple_list);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FragmentManager fm = getSupportFragmentManager();
+                if (savedInstanceState == null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .add(R.id.content_frame, new MyFragment("","#a0c0d0"))
+                            .commit();
+                }
+                fm.beginTransaction()
+                        .replace(R.id.content_frame, tabFr[position])
+                        .commit();
+                drawerLayout.closeDrawers();
+            }
+        });
+
     }
 }
--- zad1_16/src/main/java/com/example/zad1_16/MyFragment.java
+++ zad1_16/src/main/java/com/example/zad1_16/MyFragment.java
@@ -1,4 +1,55 @@
 package com.example.zad1_16;
 
-public class MyFragment {
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MyFragment extends Fragment {
+    private TextView tv;
+    private String color;
+    private String text;
+
+    public static final String COLOR = "color";
+    public static final String TEXT = "text";
+
+    MyFragment(String color, String text){
+        this.color = color;
+        this.text = text;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        tv = view.findViewById(R.id.text_fragment);
+        tv.setBackgroundColor(Color.parseColor(color));
+        tv.setText(text);
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(COLOR, color);
+        outState.putString(TEXT, text);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(savedInstanceState != null) {
+            color = savedInstanceState.getString(COLOR);
+            text = savedInstanceState.getString(TEXT);
+        }
+    }
+
 }
